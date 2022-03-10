259a9d5b69afc7b0618c8abbb5ad7ea3e977b2cf
kather0220 <78165538+kather0220@users.noreply.github.com>
2022-03-10 15:13:30 +0900

    Update 1946.cpp

--- 0304 정렬/1946.cpp
+++ 0304 정렬/1946.cpp
@@ -3,33 +3,37 @@
 #include <algorithm>
 
 using namespace std;
+typedef pair<int, int> tw;
+
+int cmp_count(int n) {
+    int count = 1;
+    vector<tw> arr(n);
+
+    for(int j=0; j<n; j++) {
+        cin >> arr[j].first >> arr[j].second;
+    }
+    sort(arr.begin() , arr.end());
+
+    int cmp = arr[0].second;
+
+
+    for(int k=1; k<n; k++) {
+        if(arr[k].second < cmp){
+            count++;
+            cmp = arr[k].second;
+        }
+    }
+    return count;
+}
 
 int main() {
     int t, n;
-
+    int count;
     cin >> t;
     for(int i=0; i<t; i++){
-
         cin >> n;
-        vector<pair<int, int>> arr(n);
-
-        for(int j=0; j<n; j++) {
-            cin >> arr[j].first >> arr[j].second;
-        }
-        sort(arr.begin() , arr.end());
-
-        int cmp = arr[0].second;
-        int count = 1;
-
-        for(int k=1; k<n; k++) {
-            if(arr[k].second < cmp){
-                count++;
-                cmp = arr[k].second;
-            }
-        }
-
+        count = cmp_count(n);
         cout << count << '\n';
     }
 }
-
 
