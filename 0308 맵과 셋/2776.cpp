68e05819dbfb75f983e13b12e99b28f45c303f07
kather0220 <78165538+kather0220@users.noreply.github.com>
2022-03-13 17:06:34 +0900

    [맵과 셋] 3월 8일 - 2776.cpp

---
+++ 0308 맵과 셋/2776.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <set>
+
+using namespace std;
+
+void find_num(set<int> &v, int M) {
+    int input;
+    while(M--) {
+        cin >> input;
+        if(v.find(input)!=v.end()){
+            cout << 1 << '\n';
+            continue;
+        } else {
+            cout << 0 << '\n';
+        }
+    }
+}
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    int T, N, M, input;
+    cin >> T;
+    while(T--) {
+        // 상대적으로 삽입 삭제가 빠른 set을 사용해줌.
+        set<int> note1;
+        cin >> N;
+        for (int i = 0; i < N; i++) {
+            cin >> input;
+            note1.insert(input);
+        }
+        cin >> M;
+        find_num(note1, M);
+    }
+    return 0;
+}
