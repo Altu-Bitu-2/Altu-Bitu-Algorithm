a65ad6f4d780bd72dc6a75dc9c858e4428e17d36
kather0220 <78165538+kather0220@users.noreply.github.com>
2022-03-14 22:49:44 +0900

    Update 19583.cpp

--- 0308 맵과 셋/19583.cpp
+++ 0308 맵과 셋/19583.cpp
@@ -1,24 +1,24 @@
 #include <iostream>
-#include <set>
+#include <map>
 
 using namespace std;
 
 int main() {
     string S, E, Q;
     cin >> S >> E >> Q;
-    set<string> arr;
+    map<string, bool> arr;
     int answer = 0;
     while(!cin.eof()) {
         string time, name;
         cin >> time >> name;
-        if(time <= S && arr.count(name) == 0){
+        if(time <= S && !arr[name]){
             // 개강 총회 시작 전에 채팅을 남긴 경우 배열에 insert
-            arr.insert(name);
-        } else if (E <= time && time <= Q && arr.count(name) == 1) {
+            arr[name] = true;
+        } else if (E <= time && time <= Q && arr[name]) {
             // 개강 총회 전에 채팅을 남겼고 개강 총회가 끝난 시간과 스트리밍이 끝난 시간 사이에 채팅 남긴 경우 출석 카운트 하고
             // 개강 총회 시작 전 채팅 목록에서 해당 이름을 삭제함.
             answer++;
-            arr.erase(name);
+            arr[name] = false;
         }
     }
 
