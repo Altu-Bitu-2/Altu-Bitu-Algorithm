fec3d2972b27c98ecd9486fde28a1d008f73312e
kather0220 <78165538+kather0220@users.noreply.github.com>
2022-03-14 22:49:04 +0900

    Update 11478.cpp

--- 0308 맵과 셋/11478.cpp
+++ 0308 맵과 셋/11478.cpp
@@ -4,19 +4,23 @@
 
 using namespace std;
 
+void string_count(string word, set<string> &sub) {
+    for(int i=0; i<word.size(); i++) {
+        string temp = "";
+        for(int j=i; j<word.size(); j++){
+            temp+=word[j];
+            sub.insert(temp);
+        }
+    }
+}
+
 int main() {
     string word;
     cin >> word;
     set<string> sub;
-
-    for(int i=0; i<word.size(); i++) {
-        string temp = "";
-        for(int j=i; j<word.size(); j++){
-            temp.push_back(word[j]);
-            sub.insert(temp);
-        }
-    }
+    string_count(word, sub);
     cout << sub.size();
     return 0;
 }
+
 
