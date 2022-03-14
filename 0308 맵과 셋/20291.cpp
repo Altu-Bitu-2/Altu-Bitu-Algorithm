5edf176fdd88ad4709618122855aa2c59a491bc8
kather0220 <78165538+kather0220@users.noreply.github.com>
2022-03-14 22:50:01 +0900

    Update 20291.cpp

--- 0308 맵과 셋/20291.cpp
+++ 0308 맵과 셋/20291.cpp
@@ -12,12 +12,7 @@ int main() {
         string file;
         cin >> file;
         string extension = file.substr(file.find('.')+1);
-
-        if(ext_count.find(extension) == ext_count.end()) {
-                ext_count[extension] = 1;
-        } else {
-            ext_count[extension]+=1;
-        }
+        ext_count[extension]++;
     }
     for(auto iter = ext_count.begin(); iter!=ext_count.end(); iter++) {
         cout << iter->first <<  ' ' << iter->second << '\n';
