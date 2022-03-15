c5ca04e59bcfb76b363686ae751264d8d4f2dd61
kather0220 <78165538+kather0220@users.noreply.github.com>
2022-03-16 01:05:10 +0900

    Update 1213.cpp

--- 0311  스택,큐,덱/1213.cpp
+++ 0311  스택,큐,덱/1213.cpp
@@ -9,8 +9,8 @@ string make_palindrome(map<char, int> &m) {
     char mid;
     stack<char> st;
     for(auto iter = m.begin(); iter!=m.end(); iter++) {
-      // 갯수가 홀수인 알파벳이 있는 경우 가운데에 놓기 위해 따로 빼둔다.
-        if(iter->second%2 == 1){
+        // 갯수가 홀수인 알파벳이 있는 경우 가운데에 놓기 위해 따로 빼둔다.
+        if(iter->second%2){
             mid = iter->first;
             iter->second --;
         }
@@ -30,15 +30,15 @@ string make_palindrome(map<char, int> &m) {
 }
 
 
-string palindrome_check(string &s) {
+string check_palindrome(string &s) {
     map<char, int> m;
     int is_ok = 0;
     for(int i=0; i<s.length(); i++) {
         m[s[i]]++;
     }
     for(auto iter = m.begin(); iter!=m.end(); iter++) {
-      // 갯수가 홀수인 알파벳이 하나보다 많으면 안되므로 is_ok 를 통해 확인해준다.
-        if(iter->second % 2 == 1) {
+        // 갯수가 홀수인 알파벳이 하나보다 많으면 안되므로 is_ok 를 통해 확인해준다.
+        if(iter->second % 2) {
             is_ok++;
         }
         if(is_ok > 1)
@@ -50,5 +50,5 @@ string palindrome_check(string &s) {
 int main() {
     string s;
     cin >> s;
-    cout << palindrome_check(s);
+    cout << check_palindrome(s);
 }
