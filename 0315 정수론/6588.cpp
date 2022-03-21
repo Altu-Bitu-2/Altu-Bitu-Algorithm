be35b38998742e5c2a299fbe9bb6ee05cc2794ce
kather0220 <78165538+kather0220@users.noreply.github.com>
2022-03-21 22:23:15 +0900

    Create 6588.cpp

---
+++ 0315 정수론/6588.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+
+using namespace std;
+
+#define MAX 1000000
+bool is_prime[MAX+1] = {true};
+
+void find_prime() {
+    is_prime[0] = is_prime[1] = false;
+    for (int i = 2; i*i <= MAX; i++) {
+        if (is_prime[i]) {
+
+            for (int j = i * i; j <= MAX; j += i) {
+                is_prime[j] = false;
+            }
+        }
+    }
+}
+
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    int n;
+    find_prime();
+    while(true) {
+        cin >> n;
+        bool flag = false;
+        if(n==0) break;
+
+        for(int i=3; i<=n; i+=2) {
+            if(is_prime[i] && is_prime[n-i]) {
+                cout << n << ' = ' << i << ' + ' << n - i << '\n';
+                flag = true;
+                break;
+            }
+        }
+        if(!flag)
+            cout << 'Goldbach\'s conjecture is wrong.' << '\n';
+    }
+    return 0;
+}
+
