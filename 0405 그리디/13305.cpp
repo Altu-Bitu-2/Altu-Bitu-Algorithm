e8d9ff21aa4342cc8941eec620f1a8788d51910e
kather0220 <78165538+kather0220@users.noreply.github.com>
2022-04-12 21:22:43 +0900

    Update 13305.cpp

--- 0405 그리디/13305.cpp
+++ 0405 그리디/13305.cpp
@@ -1,39 +1,39 @@
-#include <iostream> 
+#include <iostream>
 
-using namespace std; 
+using namespace std;
 
+int main(){
 
-int main() { 
-    
     long long oil[100000]; 
     long long road[100000]; 
-    
+
     long long city; 
     long long min; 
     long long sum; 
     
-    cin >> city; 
-    
+    cin >> city;
+
     for (int i = 0; i < city - 1; i++) { 
         cin >> road[i];
     } 
-    
+
     for (int i = 0; i < city; i++) { 
         cin >> oil[i]; 
     } 
-    
-    min = oil[0];
-    sum = min * road[0]; 
-    
-    for (int i = 0; i < city; i++) { 
-        if (min >= oil[i + 1]) { 
-            min = oil[i + 1]; 
-        } 
-        sum += min * road[i + 1]; 
-    }
-    
-    cout << sum; 
-    
-    return 0; 
-}
 
+    min = oil[0];
+    sum = min * road[0];
+
+    for (int i = 1; i < city-1; i++){
+        if (min <= oil[i]) {
+            sum += min * road[i] ;
+        }
+        else{
+            min = oil[i];
+            sum += min * road[i] ;
+        }
+    }
+    cout << sum; 
+
+    return 0;
+}
